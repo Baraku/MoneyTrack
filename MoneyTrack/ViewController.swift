fe25797edab77579080358657ef56bfcc919d719
Baraku <patrickmumme@Patricks-MacBook-Pro.local>
2015-12-28 21:39:07 +0100

    Initial Project Commit
    Empty Project Commit

---
+++ MoneyTrack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MoneyTrack
+//
+//  Created by Patrick Mumme on 12/28/15.
+//  Copyright © 2015 Patrick Mumme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
